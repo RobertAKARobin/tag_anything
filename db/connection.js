6682157f83b232eec735334fd433ddc4958f54c9
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-13 11:41:29 -0400

    Database products rendered on front-end

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+var mongoose        = require("mongoose");
+
+var ProductSchema  = {
+  name:       String
+}
+
+mongoose.model("Product", ProductSchema);
+
+mongoose.connect("mongodb://localhost/taganything");
+
+module.exports = mongoose;
